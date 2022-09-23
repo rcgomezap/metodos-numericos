cd1643581baf08b525fee877d17ec98c083e9de6
rcgomezap <rcgomezap@gmail.com>
2022-09-22 19:39:32 -0500

    Create Proyecto v0.1.py

---
+++ Proyecto/Proyecto v0.1.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 22 12:51:11 2022
+
+@author: Julian
+"""
+
+import sympy as sy
+import matplotlib.pyplot as plt
+import numpy as np
+
+k=1
+P=1
+U=1
+D=1
+o=3.8e-14
+x,y,t = sy.symbols('x y t')
+
+def T(r):
+    
+    f1 = P*np.exp(-U*r)/(4*np.pi*D*r)
+    f = o*f1/(4*np.pi*k*r)
+
+    return f
+
+def T1(x,y):
+    r=np.sqrt(x**2+y**2)
+    Tx=T(r)
+    return Tx
+
+def Tt(r,ti):
+    f1 = P*sy.exp(-U*x)/(4*sy.pi*D*x)
+    f2 = 2*(sy.erfc(x/sy.sqrt(4*k*t))*(o*f1/(4*sy.pi*k*x)))
+    f3 = sy.lambdify([x,t],f2, "numpy")
+    f=f3(r,ti)
+    return f
+
+rx=np.linspace(1e-9,2e-9,100)
+ry=rx
+rz=rx
+tiempo=np.linspace(1,20,100)
+dim=(100,100)
+Z=np.zeros(dim)
+y=np.zeros(100)
+Ttiempo=np.zeros(100)
+
+plt.figure(1)
+X, Y = np.meshgrid(rx, ry)
+Z=T1(X,Y)
+plt.pcolor(X, Y, Z)
+plt.colorbar()
+plt.show()
+
+for i in range (0,100):
+    Ttiempo[i]=Tt(3,tiempo[i])
+    
+plt.plot(tiempo,Ttiempo)
